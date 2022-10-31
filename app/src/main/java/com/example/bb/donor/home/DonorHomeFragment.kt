80942128eb6aae1184d2ea7f095bf532dbf83f40
alik13 <ali.kh1615@gmail.com>
2022-10-31 16:03:24 +0200

    Add onFocusChangeListeners

--- app/src/main/java/com/example/bb/donor/home/DonorHomeFragment.kt
+++ app/src/main/java/com/example/bb/donor/home/DonorHomeFragment.kt
@@ -1,19 +1,26 @@
 package com.example.bb.donor.home
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.findFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.bb.R
 import com.example.bb.database.BBDatabase
 import com.example.bb.databinding.FragmentDonorHomeBinding
+import com.example.bb.databinding.FragmentRegistrationBinding
 import com.example.bb.donor.adapters.CharityAdapter
 import com.example.bb.donor.adapters.CharityClickListener
+import com.google.android.material.snackbar.Snackbar
 
 class DonorHomeFragment : Fragment() {
 //    val args = DonorHomeFragmentArgs.fromBundle(requireArguments())
@@ -63,6 +70,54 @@ class DonorHomeFragment : Fragment() {
             binding.donateByCaseButton.isEnabled = false
             binding.donateByCaseCard.visibility = View.VISIBLE
         }
+
+        binding.caseNumberEdit.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+            {
+                if(!viewModel.validateCaseNumber(binding.caseNumberEdit.text.toString()).successful)
+                {
+                    viewModel.setIsCaseNumValid(false)
+                    binding.caseNumberContainer.error = viewModel.validateCaseNumber(binding.caseNumberEdit.text.toString()).errorMessage
+                }
+                else
+                {
+                    viewModel.setIsCaseNumValid(true)
+                    binding.caseNumberContainer.error = null
+                }
+            }
+        }
+
+        binding.viewCaseButton.setOnClickListener {
+            hideSoftKeyboard(context as Activity, it)
+            clearFocus(binding)
+            if(viewModel.getIsCaseNumValid()) {
+                viewModel.getCase(binding.caseNumberEdit.text.toString().toLong())
+            }
+            else
+            {
+                Snackbar.make(requireActivity().findViewById(android.R.id.content),"Invalid case number!",Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+
+        viewModel.case.observe(viewLifecycleOwner, Observer {
+            if(viewModel.case.value != null)
+            {
+                //viewModel.onViewCaseClicked(binding.caseNumberEdit.text.toString().toLong())
+                this.findNavController().navigate(DonorHomeFragmentDirections.actionDonorHomeFragmentToCaseFragment(viewModel.case.value!!.id))
+                viewModel.onCaseFragmentNavigated()
+            }
+        })
         return binding.root
+    }
+
+    private fun clearFocus(binding: FragmentDonorHomeBinding) {
+        binding.caseNumberContainer.clearFocus()
+        binding.viewCaseButton.requestFocus()
+    }
+
+    private fun hideSoftKeyboard(activity: Activity, view: View) {
+        val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.applicationWindowToken, 0)
     }
 }
