b2365a273585f6bab1674a64da935bb5417e7f5a
alik13 <ali.kh1615@gmail.com>
2022-10-31 16:02:42 +0200

    Add item decoration

--- app/src/main/java/com/example/bb/donor/charity/CharityFragment.kt
+++ app/src/main/java/com/example/bb/donor/charity/CharityFragment.kt
@@ -1,24 +1,22 @@
 package com.example.bb.donor.charity
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.bb.R
 import com.example.bb.database.BBDatabase
 import com.example.bb.databinding.FragmentCharityBinding
 import com.example.bb.donor.adapters.CaseAdapter
 import com.example.bb.donor.adapters.CaseClickListener
-import com.example.bb.donor.adapters.CharityAdapter
-import com.example.bb.donor.adapters.CharityClickListener
 import com.squareup.picasso.Picasso
+
 
 class CharityFragment : Fragment() {
 
@@ -51,6 +49,12 @@ class CharityFragment : Fragment() {
             }
         })
 
+        binding.caseList.addItemDecoration(
+            DividerItemDecoration(
+                binding.caseList.context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
         binding.caseList.adapter = adapter
 
         viewModel.navigateToCaseFragment.observe(this.viewLifecycleOwner, Observer { id ->
