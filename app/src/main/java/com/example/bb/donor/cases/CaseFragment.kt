5b4d73d30f5c776116d311af9a1ed35e9690a973
alik13 <ali.kh1615@gmail.com>
2022-10-31 16:01:02 +0200

    Add donation custom dialog

--- app/src/main/java/com/example/bb/donor/cases/CaseFragment.kt
+++ app/src/main/java/com/example/bb/donor/cases/CaseFragment.kt
@@ -1,16 +1,19 @@
 package com.example.bb.donor.cases
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.bb.R
 import com.example.bb.database.BBDatabase
 import com.example.bb.databinding.FragmentCaseBinding
+import com.google.android.material.textfield.TextInputLayout
 import com.squareup.picasso.Picasso
 
 class CaseFragment : Fragment() {
@@ -22,7 +25,7 @@ class CaseFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val args = CaseFragmentArgs.fromBundle(requireArguments())
         // Inflate the layout for this fragment
         val binding: FragmentCaseBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_case,container,false)
@@ -34,7 +37,7 @@ class CaseFragment : Fragment() {
         binding.lifecycleOwner = this
         viewModel.onRetrieveCase(args.caseId)
 
-        viewModel.case.observe(this.viewLifecycleOwner, Observer {
+        viewModel.case.observe(this.viewLifecycleOwner) {
             viewModel.retrieveCharity(it.charityId)
             binding.caseNumText.text = viewModel.case.value!!.id.toString()
             binding.caseDescriptionText.text = viewModel.case.value!!.description
@@ -42,11 +45,69 @@ class CaseFragment : Fragment() {
             binding.amountNeddedText.text = viewModel.case.value!!.donationsNeeded.toString()
             binding.amountPaidText.text = viewModel.case.value!!.donationsPaid.toString()
             binding.criticalityLevelText.text = viewModel.case.value!!.criticality
-        })
+        }
 
-        viewModel.charity.observe(this.viewLifecycleOwner, Observer {
-            Picasso.get().load(viewModel.charity.value!!.image).placeholder(R.drawable.ic_launcher_background).into(binding.charityLogoC)
-        })
+        viewModel.charity.observe(this.viewLifecycleOwner) {
+            Picasso.get().load(viewModel.charity.value!!.image)
+                .placeholder(R.drawable.ic_launcher_background).into(binding.charityLogoC)
+        }
+
+        binding.donateButton.setOnClickListener {
+            showCustomDialog(viewModel)
+        }
         return binding.root
+    }
+
+    private fun showCustomDialog(viewModel: CaseViewModel) {
+        val dialog = Dialog(this.context!!)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.donation_dialog)
+
+        val minButton: Button = dialog.findViewById(R.id.min_button)
+        val maxButton: Button = dialog.findViewById(R.id.max_button)
+        val donateButton: Button = dialog.findViewById(R.id.d_donate_button)
+        val dAmountContainer: TextInputLayout = dialog.findViewById(R.id.donation_container)
+
+        minButton.setOnClickListener {
+            if(viewModel.case.value != null)
+            {
+                if(viewModel.case.value!!.donationsNeeded>0)
+                {
+                    dAmountContainer.editText!!.setText("1.0")
+                }
+                else
+                {
+                    dAmountContainer.editText!!.setText("0.0")
+                }
+            }
+        }
+
+        maxButton.setOnClickListener {
+            if(viewModel.case.value != null) {
+                dAmountContainer.editText!!.setText(viewModel.case.value!!.donationsNeeded.toString())
+            }
+        }
+
+        donateButton.setOnClickListener {
+            dialog.dismiss()
+            //viewModel.updateCase(dAmountEdit.text.toString().toDouble())
+            showFinishDialog()
+        }
+
+        dialog.show()
+    }
+    private fun showFinishDialog()
+    {
+        val dialog = Dialog(this.context!!)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.success_dialog)
+        val closeButton: Button = dialog.findViewById(R.id.close_button)
+
+        closeButton.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
